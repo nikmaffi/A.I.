551c5402a9b5a031e74bc2a9d6d2b0e6e0fc3e5c
Nicolò Maffi <nik02.maffi@gmail.com>
2022-04-05 16:49:46 +0200

    Add files via upload

--- perceptron/lib/neuron.py
+++ perceptron/lib/neuron.py
@@ -1,34 +1,45 @@
-import math
 import numpy
 import functools
 
 class Neuron:
-    def __init__(self, nSynapses, trainingFrequency, learningRate):
-        self.trainingFrequency = trainingFrequency
-        self.learningRate = learningRate
+    def __init__(self, synapses, learning_rate, active_func):
+        self.learning_rate = learning_rate
+        self.synapses = synapses
+        self.active_func = active_func
 
-        self.nSynapses = nSynapses
-        self.weights = numpy.random.random(nSynapses)
+        self.weights = numpy.random.random(synapses)
         self.bias = numpy.random.random()
 
-    def active(self, inputs):
-        inputs = numpy.array(inputs, dtype=float)
+    def __active(self, inputs):
         sum = functools.reduce(lambda s, el: s + el[0] * el[1], zip(self.weights, inputs),  0) + self.bias
 
-        return 1 / (1 + math.exp(-sum))
+        return self.active_func(sum)
 
-    def __derivative(self, inputs, objective, relativeDerivative):
-        sigm = self.active(inputs)
+    def __derivative(self, inputs, output_expected, relativ_derivative):
+        func = self.__active(inputs)
 
-        return 2 * (sigm - objective) * (sigm * (1 - sigm)) * relativeDerivative
+        return 2 * (func - output_expected) * (func * (1 - func)) * relativ_derivative
 
-    def fit(self, inputs, objectives):
-        inputs = numpy.array(inputs, dtype=float)
-        objectives = numpy.array(objectives, dtype=float)
+    def score(self, inputs, expected_outputs):
+        outputs = self.predict(inputs)
 
-        for _ in range(self.trainingFrequency):
+        corrects = (outputs.round() == expected_outputs).sum()
+        totals = len(expected_outputs)
+
+        return corrects / totals
+
+    def predict(self, inputs_set):
+        outputs = numpy.zeros(len(inputs_set))
+
+        for i, inputs in enumerate(inputs_set):
+            outputs[i] = self.__active(inputs)
+
+        return outputs
+
+    def fit(self, inputs, output_expected, training_frequency):
+        for _ in range(training_frequency):
             ti = numpy.random.randint(0, len(inputs))
 
-            for i in range(self.nSynapses):
-                self.weights[i] = self.weights[i] - self.learningRate * self.__derivative(inputs[ti], objectives[ti], inputs[ti][i])
-            self.bias = self.bias - self.learningRate * self.__derivative(inputs[ti], objectives[ti], 1)
+            for i in range(self.synapses):
+                self.weights[i] = self.weights[i] - self.learning_rate * self.__derivative(inputs[ti], output_expected[ti], inputs[ti][i])
+            self.bias = self.bias - self.learning_rate * self.__derivative(inputs[ti], output_expected[ti], 1)
